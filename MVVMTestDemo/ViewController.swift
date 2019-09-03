718a4f9a62bc6d7c13d8b304d6d619a2db162e51
Ravi Parsa <ravi.parsa@mobileprogrammingllc.com>
2019-09-03 23:11:35 +0530

    Initial Commit

---
+++ MVVMTestDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMTestDemo
+//
+//  Created by Ravi Parsa on 03/09/19.
+//  Copyright © 2019 Ravi Parsa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
